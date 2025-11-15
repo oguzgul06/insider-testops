c0042df1ecb91af31a1c03c5c899462f05106e2f
o00877468 <oguz.gul1@huawei.com>
2025-11-15 16:19:41 +0300

    bekleme suresini artirdim ve JS ile yaptim

--- tests/test_careers.py
+++ tests/test_careers.py
@@ -33,17 +33,20 @@ def test_careers_page():
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get("https://useinsider.com/")
 
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.LINK_TEXT, "Company"))
-    ).click()
+    company_link = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Company"))
+    )
+    driver.execute_script("arguments[0].click();", company_link)
 
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.LINK_TEXT, "Careers"))
-    ).click()
+    careers_link = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Careers"))
+    )
+    driver.execute_script("arguments[0].click();", careers_link)
 
     assert "Careers" in driver.title
     driver.quit()
